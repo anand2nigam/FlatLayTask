c3f1722cd496dda9a9f390d0606cbc7a7cafe381
Anand Nigam <anandnigam.2aug@gmail.com>
2018-09-16 23:20:20 +0530

    Basic UI added. Choosing the image from the image source functionality implemented. Images can be editable.

---
+++ FlatLayTask/Controller/ImageDisplayTableViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ImageDisplayTableViewController.swift
+//  FlatLayTask
+//
+//  Created by Anand Nigam on 16/09/18.
+//  Copyright © 2018 Anand Nigam. All rights reserved.
+//
+
+import UIKit
+
+class ImageDisplayTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imagesGallery: [UIImage] = [ ]
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBAction func addImageButtonTapped(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Choose Image Source", message: "", preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (alertAction) in
+                self.imagePicker.sourceType = .camera
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+            alert.addAction(cameraAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (alertAction) in
+                self.imagePicker.sourceType = .photoLibrary
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+            alert.addAction(photoLibraryAction)
+        }
+        
+         alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+        
+        imagePicker.allowsEditing = true
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - TableView DataSource Methods
+
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return imagesGallery.count
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath)
+//        
+//        
+//        
+//    }
+//   
+
+}
